dfa191ba1e22c91f84abbeb63ffa81e78b92c3da
Akshat Jaitly <akshatjaitly@gmail.com>
2022-07-02 14:06:49 +0530

    added new judge0

---
+++ new  judge0/lang_imports/edgar_langs/node.rb
@@ -0,0 +1,10 @@
+@edgar_langs ||= []
+@edgar_langs += 
+[
+    {
+      "name": "nomongo",
+      "is_archived": false,
+      "source_file": "Main.js",
+      "run_cmd": "NODE_PATH=/usr/lib/node_modules node  Main.js"
+    }
+  ]
